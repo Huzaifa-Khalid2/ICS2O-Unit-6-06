c3febcd393de28d4e0d9f3d77b5259f2b2f9516b
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-07 16:15:32 +0000

    fixed a lot

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('my-btn').addEventListener('click', myFunction)
 function myFunction () {
- let age = document.getElementById('my-text').value
+  let age = document.getElementById('my-text').value
   age = parseInt(age)
- if (age == '15') {
+  if (age === '15') {
     alert('congratulations you guessed right!')
   } else {
     alert('Sorry you guessed worng:( better luck next time!')
