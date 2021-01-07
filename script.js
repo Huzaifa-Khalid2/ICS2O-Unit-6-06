f5a9cfb84a67cc8a217985d21f116a7958274545
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-07 16:09:42 +0000

    fixed and edited a lot

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('my-btn').addEventListener('click', myFunction)
+function myFunction () {
+ let age = document.getElementById('my-text').value
+  age = parseInt(age)
+ if (age == '15') {
+    alert('congratulations you guessed right!')
+  } else {
+    alert('Sorry you guessed worng:( better luck next time!')
+  }
+}
